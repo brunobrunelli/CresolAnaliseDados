6942704b99184fc0b04132a93ab3082f25ad753a
Bruno Scovoli <brunobrunelli.bb@gmail.com>
2019-05-27 14:43:24 -0300

    Correção de escrita e estrutura

--- Projeto_Cresol_Analise_Dados/Python/AnaliseDadosLogCresol.py
+++ Projeto_Cresol_Analise_Dados/Python/AnaliseDadosLogCresol.py
@@ -19,19 +19,19 @@ import pandas as pd
 import os
 from datetime import datetime 
 
-# Função para extração e análise dos dados do arquivo de log.gz:
-def extrair_analisar_dados_arquivo():
-    
+# Extração de dados do arquivo de log.gz:
+def extrair_dados_arquivo():
     # Criação de um dataframe a partir da leitura de dados de um arquivo.log:
     df_data = pd.read_csv('apache.log.gz',compression = 'gzip',sep = " ",header = None,\
         names = ['ip_address', 'ifen', 'user', 'user_code', 'timestamp','request', 'html_code',\
          'size_object', 'url_refer', 'browser']
         ).drop(columns=['ifen'])
-    
-    
-    # ----------------------------------------------------------------------------------------------------------------
-    # Reestruturação e modificação das colunas do dataframe:
 
+    return df_data
+
+# Reestruturação e modificação das colunas do dataframe:
+def modifica_dados_dataframe(df_data):
+    
     # Formatação da coluna de data e hora:
     df_data.timestamp = pd.to_datetime(df_data.timestamp, format='[%Y-%m-%dT%H:%M:%SZ]')
     
@@ -40,55 +40,77 @@ def extrair_analisar_dados_arquivo():
 
     # Extraindo  
     df_data.browser = df_data.browser.apply(lambda x: x.split(" ")[0])
+    
+    return df_data
 
-    # ----------------------------------------------------------------------------------------------------------------
-    # Lista de dados esperado como saída:
-
-    # os 5 (cinco) logins que mais efetuaram requisições;
+# Função para extração e análise dos dados do arquivo de log.gz:
+def analisar_dados_dataframe(df_data):
+    
+    # -----------------------------------------------------------------------------------------------------------------
+    
+    # Realizamos um Groupby do dataframe pela coluna "USER" retornando em formato de lista:
     top_logins = df_data.groupby(by='user').size().sort_values(ascending=False).head().reset_index().user
     top_logins = list(top_logins)
-
-    # os 10 (dez) browsers mais utilizados;
+    
+    # Realizamos um Groupby do dataframe pela coluna "BROWSER" retornando os 10 primeiros registros em formato de lista:
     top_browsers = df_data.groupby(by='browser').size().sort_values(ascending=False).head(10).reset_index().browser
     top_browsers = list(top_browsers)
 
-    # os endereços de rede (classe C) com maior quantidade de requisições;
+    # Realizamos um Groupby do dataframe pela coluna "IP_ADDRESS" retornando os ips de classe "C" em formato de lista:
     top_endereco_rede = df_data.loc[df_data.ip_address.apply(lambda x: int(x.split('.')[0]) >= 192 and int(x.split('.')[0]) <= 224)]\
     .groupby(by='ip_address').size().sort_values(ascending=False).reset_index().ip_address
     top_endereco_rede = list(top_endereco_rede)
 
-    # a hora com mais acesso no dia;
+    # Realizamos um resample do dataframe pela "HORA" retornando a hora de mais acesso no dia:
     hora_acesso_dia = df_data.resample('H').size().sort_values(ascending=False).index[0].hour
 
-    # a hora com a maior quantidade de bytes;
+    # Realizamos um resample do dataframe pela "HORA" retornando a hora de maior tamanho em bytes:
     hora_bytes = df_data.size_object.resample('H').sum().sort_values(ascending=False).index[0].hour
     
-    # o endpoint com maior consumo de bytes;
+    # Realizamos um Groupby do dataframe pela coluna "ENDPOINT" retornando o endpoint de maior tamanho:
     maior_endpoint = df_data.assign(endpoint = df_data.request.apply(lambda x: x.split("/")[1]))\
     .groupby('endpoint').size().sort_values(ascending=False).index[0]
     
-    # a quantidade de bytes por minuto;
+    # Realizamos um resample do dataframe pelo "MINUTO" retornando a quantidade de minutos de maior tamanho:
     qtd_bytes_min = df_data.size_object.resample('Min').sum().mean()
-
-    # a quantidade de bytes por hora;
+    
+    # Realizamos um resample do dataframe pelo "HORA" retornando a quantidade de horas de maior tamanho:
     qtd_bytes_hora = df_data.size_object.resample('H').sum().mean()
 
-    # a quantidade de usuários por minuto;
+    # Realizamos um resample do dataframe pelo "MINUTO" retornando a quantidade de minutos por usuário:
     qtd_user_min = df_data.user.resample('Min').size().mean()
 
-    # a quantidade de usuários por hora;
+    # Realizamos um resample do dataframe pelo "HORA" retornando a quantidade de horas por usuário:
     qtd_user_hora = df_data.user.resample('H').size().mean()
 
-    # a quantidade de requisições que tiveram erro de cliente, agrupadas por erro;
-    qtd_rec_by_cliente = df_data.loc[df_data.html_code.apply(lambda x: int(x)>= 400 and int(x) <= 499)]\
+    # Realizamos um Groupby do dataframe pela coluna "HTML_CODE" retornando dicionario com "erro de cliente":
+    qtd_req_by_cliente = df_data.loc[df_data.html_code.apply(lambda x: int(x)>= 400 and int(x) <= 499)]\
     .groupby('html_code').size().sort_values(ascending=False).to_dict()
 
-    # a quantidade de requisições que tiveram sucesso;
+    # Realizamos um Groupby do dataframe pela coluna "HTML_CODE" retornando dicionario com "sucessos":
     qtd_req_sucesso = df_data.loc[df_data.html_code.apply(lambda x: int(x) >= 200 and int(x) <= 226 )].shape[0]
 
-    # a quantidade de requisições que foram redirecionadas;
+    # Realizamos um Groupby do dataframe pela coluna "HTML_CODE" retornando dicionario com "redirecionadas":
     qtd_req_redirecionada = df_data.loc[df_data.html_code.apply(lambda x: int(x)>= 300 and int(x) <= 308)].shape[0]
-
+    
+    # -----------------------------------------------------------------------------------------------------------------
+    
+    # Lista de dados esperado como saída:
+    # os 5 (cinco) logins que mais efetuaram requisições;
+    # os 10 (dez) browsers mais utilizados;
+    # os endereços de rede (classe C) com maior quantidade de requisições;
+    # a hora com mais acesso no dia;
+    # a hora com a maior quantidade de bytes;
+    # o endpoint com maior consumo de bytes;
+    # a quantidade de bytes por minuto;
+    # a quantidade de bytes por hora;
+    # a quantidade de usuários por minuto;
+    # a quantidade de usuários por hora;
+    # a quantidade de requisições que tiveram erro de cliente, agrupadas por erro;
+    # a quantidade de requisições que tiveram sucesso;
+    # a quantidade de requisições que foram redirecionadas;
+    
+    
     df_output = pd.DataFrame(
         {
         'top_logins': [top_logins],
@@ -101,7 +123,7 @@ def extrair_analisar_dados_arquivo():
         'qtd_bytes_hora': [qtd_bytes_hora],
         'qtd_user_min': [qtd_user_min],
         'qtd_user_hora': [qtd_user_hora],
-        'qtd_rec_by_cliente': [qtd_rec_by_cliente],
+        'qtd_req_by_cliente': [qtd_req_by_cliente],
         'qtd_req_sucesso': [qtd_req_sucesso],
         'qtd_req_redirecionada': [qtd_req_redirecionada]
         }
@@ -117,7 +139,9 @@ def grava_dados_csv(lista_dados):
 print('Processo Iniciado - %s ...\n' %str(datetime.now()))
 
 # Chamada das funções criadas acima, passando os parametros esperados:
-dados_extraidos = extrair_analisar_dados_arquivo()
-grava_dados_csv(dados_extraidos)
+df = extrair_dados_arquivo()
+df = modifica_dados_dataframe(df)
+lista = analisar_dados_dataframe(df)
+grava_dados_csv(lista)
 
 print('Processo Conclído - %s ...' %str(datetime.now()))
